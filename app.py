132ccc915151ab44eefc32e8647a01de714ebf7f
Zoha-Khan123 <zohakhan11004@gmail.com>
2025-03-04 01:19:59 +0500

    password-strength-unit

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import re
+import math
+import random
+
+def check_password_strength(password):
+    score = 0
+    
+    # Length Check
+    if len(password) >= 8:
+        score += 1
+    else:
+        st.write("❌ Password should be at least 8 characters long.")
+    
+    # Upper & Lowercase Check
+    if re.search(r"[A-Z]" , password) and re.search(r"[a-z]", password):
+        score += 1
+    else:
+        st.write("❌ Include both uppercase and lowercase letters.")
+    
+    # Digit Check
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        st.write("❌ Add at least one number (0-9).")
+    
+    # Special Character Check
+    if re.search(r"[!@#$%^&*]", password):
+        score += 1
+    else:
+        st.write("❌ Include at least one special character (!@#$%^&*).")
+    
+    # Strength Rating
+    if score == 4:
+        st.write("✅ Strong Password!")
+    elif score == 3:
+        st.write("⚠️ Moderate Password - Consider adding more security features.")
+    else:
+       st.write("❌ Weak Password - Improve it using the suggestions above.")
+
+# Generate Random Password
+option = ["Pq7$kL@3","X9yT$2Lm","A1@kFp%3","Z2fL@p7T","mL9$Xq1K"]
+choose = math.floor(random.random()*len(option))
+random = option[choose]
+
+# Input field for password
+password = st.text_input("Enter your password:",type="password")
+
+# Button to check password strength
+if st.button("Check Password Strength"):
+    if password:
+        check_password_strength(password)
+    else:
+        st.write("💡 Enter a Password? Suggest this one:", random)
+
+
+
+
